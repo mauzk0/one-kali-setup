4e745298b7976c22e425c82dceda0e082f90c9a8
mauzk0 <mr.mauz@protonmail.com>
2024-09-18 22:57:38 +0200

    Added a lot

--- one-kali-setup.sh
+++ one-kali-setup.sh
@@ -10,8 +10,35 @@ system_update() {
 # Install tools
 install_tools() {
     echo "Installing tools:"
-    sudo apt install zaproxy gobuster cyberchef seclists horst
+    sudo apt install \
+	    realtek-rtl88xxau-dkms \
+	    neofetch \
+	    zaproxy \
+	    gobuster \
+	    cyberchef \
+	    seclists \
+	    horst \
+	    nuclei \
+	    subfinder \
+	    httpx-toolkit \
+	    naabu \
+	    bettercap \
+	    beef-xss \
+	    golang \
+	    powershell
+    # Install golang packages
+    go install github.com/projectdiscovery/cvemap/cmd/cvemap@latest
+    go install github.com/projectdiscovery/katana/cmd/katana@latest
+    # Add path for Go binaries
+    export PATH=$PATH:/home/kali/go/bin
+    # Git tools and packages
+    mkdir ~/Git
+    git clone https://github.com/projectdiscovery/nuclei-templates.git
+    git clone https://github.com/peass-ng/PEASS-ng.git
     echo "Tools installed"
+    # Tools adjustments and preparations
+    msfdb init
+    sudo usermod -aG kismet kali
 }
 
 # SSH key re-generation
@@ -24,6 +51,17 @@ ssh-key-reconf() {
     # sudo md5sum /etc/ssh/old_keys/ssh_host_*
     # sudo md5sum /etc/ssh/ssh_host_*
     echo "SSH keys reconfiguration done"
+}
+
+# SSH and Fail2Ban
+setup_ssh_fail2ban() {
+    sudo systemctl enable ssh
+    sudo systemctl start ssh
+    echo "Instalation and setting up Fail2Ban"
+    sudo apt install fail2ban
+    sudo systemctl enable fail2ban
+    sudo systemctl start fail2ban
+    # add ufw?
 }
 
 # Install VSCode
@@ -44,6 +82,15 @@ vscode_install() {
     echo "VSCode installed"
 }
 
+# Install Nessus
+nessus_install() {
+    echo "Installing Nessus"
+    nessus_latest_deb=$(curl -s https://www.tenable.com/downloads/api/v1/public/pages/nessus | grep -Po 'Nessus-\d+\.\d+\.\d+-debian10_amd64\.deb' | head -n 1)
+    curl -o ~/Downloads/$nessus_latest_deb --request GET https://www.tenable.com/downloads/api/v2/pages/nessus/files/$nessus_latest_deb
+    sudo dpkg -i $nessus_latest_deb
+    echo "Nessus Installed"
+}
+
 # .zshrc additions
 zshrc_additions() {
     echo ".zshrc file additions"
@@ -55,9 +102,15 @@ zshrc_additions() {
 tmux_config() {
     echo "tmux configuration"
     sudo apt install xsel
-    git clone https://github.com/mauzk0/one-tmux-conf.git ~/Downloads/Git/one-tmux-conf
-    cp ~/Downloads/Git/one-tmux-conf/.tmux.conf ~/
+    git clone https://github.com/mauzk0/one-tmux-conf.git ~/Git/one-tmux-conf
+    ln -s ~/Git/one-tmux-conf/.tmux.conf ~/.tmux.conf
     echo "tmux configuration ready"
+}
+
+# clean
+clean() {
+    sudo apt autoclean
+    sudo apt autoremove
 }
 
 # Script execution
@@ -65,7 +118,10 @@ echo "Starting additional Kali tools installation and configuration"
 system_update
 install_tools
 ssh-key-reconf
+# setup_ssh_fail2ban
 vscode_install
+nessus_install
 zshrc_additions
 tmux_config
+clean
 echo "Script finished"
