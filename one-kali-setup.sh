9a297f22931bb53af89b6e48fbe88da136d7ed7e
mauzk0 <mr.mauz@protonmail.com>
2024-10-01 08:53:42 +0200

    new tools

--- one-kali-setup.sh
+++ one-kali-setup.sh
@@ -11,21 +11,27 @@ system_update() {
 install_tools() {
     echo "Installing tools:"
     sudo apt install \
-	    realtek-rtl88xxau-dkms \
-	    neofetch \
-	    zaproxy \
-	    gobuster \
-	    cyberchef \
-	    seclists \
-	    horst \
-	    nuclei \
-	    subfinder \
-	    httpx-toolkit \
-	    naabu \
-	    bettercap \
-	    beef-xss \
-	    golang \
-	    powershell
+        realtek-rtl88xxau-dkms \
+        autoconf \
+        automake \
+        ufw \
+        yersinia \
+        neofetch \
+        fail2ban \
+        zaproxy \
+        gobuster \
+        cyberchef \
+        seclists \
+        horst \
+        nuclei \
+        subfinder \
+        httpx-toolkit \
+        naabu \
+        bettercap \
+        beef-xss \
+        golang \
+        powershell \
+	sipvicious
     # Install golang packages
     go install github.com/projectdiscovery/cvemap/cmd/cvemap@latest
     go install github.com/projectdiscovery/katana/cmd/katana@latest
@@ -54,30 +60,39 @@ ssh-key-reconf() {
     echo "SSH keys reconfiguration done"
 }
 
-# SSH and Fail2Ban
-setup_ssh_fail2ban() {
+# SSH, RDP and Fail2Ban
+remote_access() {
+    sudo apt install \
+        xrdp \
+        ufw
     sudo systemctl enable ssh
     sudo systemctl start ssh
-    echo "Instalation and setting up Fail2Ban"
+    sudo systemctl enable xrdp
+    sudo systemctl start xrdp
+    # Instalation and setting up Fail2Ban
     sudo apt install fail2ban
     sudo systemctl enable fail2ban
     sudo systemctl start fail2ban
-    # add ufw?
+    # add ufw with allowed SSH access
+    sudo ufw allow OpenSSH
+    sudo ufw enable
 }
 
 # Install VSCode
 vscode_install() {
     echo "VSCode installation"
     # MS apt repository and key manual installation
-    sudo apt install wget gpg
+    sudo apt install \
+        wget \
+        gpg
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
     rm -f packages.microsoft.gpg
     # Update the package cache and install the package using
-    sudo apt install apt-transport-https
-    sudo apt clean && sudo apt autoclean && sudo apt autoremove
-    sudo apt update
+    sudo apt install \
+        apt-transport-https
+    sudo apt clean && sudo apt autoclean && sudo apt autoremove && sudo apt update
     sudo apt install code # or code-insiders
     # Set vscode as default editor
     sudo update-alternatives --set editor /usr/bin/code
@@ -119,7 +134,7 @@ echo "Starting additional Kali tools installation and configuration"
 system_update
 install_tools
 ssh-key-reconf
-# setup_ssh_fail2ban
+remote_access
 vscode_install
 nessus_install
 zshrc_additions
